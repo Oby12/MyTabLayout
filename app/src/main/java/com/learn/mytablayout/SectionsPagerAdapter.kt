7e35ef60f178da1abb750218dd50420636643baf
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-09-14 14:27:27 +0700

    membuat TabLayout cukup dengan satu Fragment, yaitu HomeFragment. Anda juga sudah dapat mengirimkan data posisi ke Fragment sehingga menghasilkan tampilan yang berbeda.

--- app/src/main/java/com/learn/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/learn/mytablayout/SectionsPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.learn.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -8,20 +9,30 @@ class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(a
 
     //Unutuk hal ini anda dapat menggunakan recycleView.Adapter
     override fun getItemCount(): Int {
-        return  2
+        return  3
         //Fungsi getItemCount digunakan untuk menentukan jumlah tab yang ingin ditampilkan. Pada kode di atas, Anda mencoba untuk menampilkan
         //dua tab. Pastikan bahwa jumlah yang ditampilkan sesuai dengan Fragment yang didefinisikan oleh fungsi createFragment
     }
 
     //Fungsi createFragment digunakan untuk menampilkan fragment sesuai dengan posisi tab-nya.
    // Misalnya kode di bawah untuk posisi 0 (tab pertama) menampilkan HomeFragment dan di posisi 1 (tab kedua) menampilkan ProfileFragment
-    override fun createFragment(position: Int): Fragment {
+    /*override fun createFragment(position: Int): Fragment {
        var fragment : Fragment? = null
         when (position){
             0 -> fragment = HomeFragment()
             1 -> fragment = ProfileFragment()
         }
         return  fragment as Fragment
+    }*/
+    //kode yang  di block di atas adalah kode yang tab layout menggunakan 2 fragment
+
+    //ini adlah kode pembuatan fragment yang hanya menggunakan 1 fragment
+    override fun createFragment(position: Int): Fragment {
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+        }
+        return fragment
     }
 
 }
